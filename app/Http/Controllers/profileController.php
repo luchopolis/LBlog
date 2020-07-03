9dbde3dc1c80978d614abdf06beba2e3d30acdc2
CaballeroWhite <ugbcaballero@gmail.com>
2020-07-02 22:51:57 -0600

    Heroku Push

--- app/Http/Controllers/profileController.php
+++ app/Http/Controllers/profileController.php
@@ -20,8 +20,8 @@ class profileController extends Controller
      */
     public function index(User $user)
     {
-        $this->middleware('Auth');
-        
+        $this->middleware('auth');
+
         $profile = profile::where('user_id','=', $user->id)->get();
 
     
