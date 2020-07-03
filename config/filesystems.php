39204605b059499b6f6059fc152a8643c19dcfb5
CaballeroWhite <ugbcaballero@gmail.com>
2020-07-02 20:57:22 -0600

    Heroku cambio

--- config/filesystems.php
+++ config/filesystems.php
@@ -86,7 +86,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        public_path('storageD') => storage_path('app/public'),
         public_path('imagesPost') => storage_path('app/postImages')
     ],
 
